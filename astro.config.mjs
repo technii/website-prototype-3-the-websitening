80f4fe3506e628480bb43d67116f91802e7c4224
techni <lolerlol1234@gmail.com>
2025-11-20 13:34:42 +0000

    hopefully it pushes to live

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import awsAmplify from 'astro-aws-amplify';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "technicallycolours.com",
+    adapter: awsAmplify()
+});
+
+
