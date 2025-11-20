b1f27c9c9a10fc4f326c68ff89c66241a0509fbb
techni <lolerlol1234@gmail.com>
2025-11-20 14:07:27 +0000

    -added package lock file so it might work now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import awsAmplify from 'astro-aws-amplify';
 // https://astro.build/config
 export default defineConfig({
-    site: "technicallycolours.com",
     adapter: awsAmplify(),
     output: "server"
 });
